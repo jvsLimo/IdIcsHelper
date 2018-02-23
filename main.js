eab360c042d18c127559799669663500e6bd8b75
jvsLimo <linkyos@free.fr>
2018-02-23 23:52:09 +0100

    First stable build

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function createWindow () {
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', () {
+app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') {
         app.quit()
     }
